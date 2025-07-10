605af263d0de2dba5de67caa552048f5c8208441
YURI SEVERO PINTO COSTA <6229121@cbmpa.local>
2025-07-10 16:13:00 -0300

    update existing features

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ app.use(express.json());
 
 app.post("/preencher-pdf", async (req, res) => {
   try {
-    const {tipo_evento, id, protocolo, data_recebimento, tecnico_recebedor, nome_solicitante, avaria_descricao, equipamento} = req.body;
+    const { evento, equipamento } = req.body;
 
-    if(tipo_evento === "NovoRecebimento") {
-      console.log("Evento de Novo Recebimento recebido");
-    }
     // Lê o PDF original
-    const pdfPath = "./termo-recebimento-editavel-template.pdf";
+    const pdfPath = "./termo-template.pdf";
     const existingPdfBytes = fs.readFileSync(pdfPath);
 
     // Carrega o documento PDF
@@ -22,23 +19,52 @@ app.post("/preencher-pdf", async (req, res) => {
     // Permite edição de formulários
     const form = pdfDoc.getForm();
 
-    // Preenche os campos com os dados recebidos
-    form.getTextField("protocolo_field").setText(protocolo);
-    form.getTextField("setor_field").setText(equipamento.setor);
-    form.getTextField("equipamento_field").setText(equipamento.tipo_equipamento);
-    form.getTextField("numero_serie_field").setText(equipamento.numero_serie);
-    form.getTextField("registro_patrimonio_field").setText(equipamento.registro_patrimonio);
-    form.getTextField("obs_field").setText(`
-      Memoria RAM(Gb): ${equipamento.memoria_ram_gb}, Processador: ${equipamento.processador}, 
-      DVD: ${equipamento.dvd}, Fonte: ${equipamento.fonte}, Placa Mae: ${equipamento.placa_mae},
-      Placa Wi-Fi: ${equipamento.placa_wifi}, Antena: ${equipamento.antena},
-      Cabo de Forca: ${equipamento.cabo_fonte}, N/S cabo de Forca: ${equipamento.numero_serie_cabo_fonte},
-      `);
+    if (evento.tipo === "Recebimento") {
+      console.log("Novo evento de Recebimento recebido");
+      form.getTextField("tipo_verbo_field").setText("recebido");
+    }
     
-    form.getTextField("nome_solicitante_field").setText(nome_solicitante);
-    form.getTextField("tecnico_recebedor_field").setText(tecnico_recebedor);
-    form.getTextField("avaria_descricao_field").setText(avaria_descricao);
-    form.getTextField("data_recebimento_field").setText(data_recebimento);
+    if (evento.tipo === "Devolucao") {
+      console.log("Novo evento de Devolução recebido");
+      form.getTextField("tipo_verbo_field").setText("devolvido");
+    }
+
+    if (evento.tipo === "Cautela") {
+      console.log("Novo evento de Cautela recebido");
+      form.getTextField("tipo_verbo_field").setText("cautelado");
+    }
+    
+    if (evento.tipo === "Descautela") {
+      console.log("Novo evento de Descautela recebido");
+      form.getTextField("tipo_verbo_field").setText("descautelado");
+    }
+    
+    form.getTextField("tipo_evento_field").setText(evento.tipo.toUpperCase());
+    
+    form.getTextField("protocolo_field").setText(evento.protocolo);
+   
+    form
+      .getTextField("tipo_equipamento_field")
+      .setText(equipamento.tipo_equipamento);
+    
+    form.getTextField("numero_serie_field").setText(equipamento.numero_serie);
+    
+    form
+      .getTextField("registro_patrimonio_field")
+      .setText(equipamento.registro_patrimonio);
+    
+    form.getTextField("nome_entregador_field").setText(evento.nome_entregador);
+    form.getTextField("nome_receptor_field").setText(evento.nome_receptor);
+    form.getTextField("data_field").setText(evento.data);
+    
+    form.getTextField("observacao_field").setText(`
+    Memoria RAM(Gb): ${equipamento.memoria_ram_gb}, Processador: ${equipamento.processador}, 
+    DVD: ${equipamento.dvd}, Fonte: ${equipamento.fonte}, Placa Mae: ${equipamento.placa_mae},
+    Placa Wi-Fi: ${equipamento.placa_wifi}, Antena: ${equipamento.antena},
+    Cabo de Forca: ${equipamento.cabo_fonte}, N/S cabo de Forca: ${equipamento.numero_serie_cabo_fonte},
+    `);
+
+    
 
     // Flatteia o formulário (aplica os valores no PDF)
     form.flatten();
